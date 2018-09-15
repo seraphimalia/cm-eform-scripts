43845bfaaa4a5cedbb36a159d2483babc1221dc8
Clive Munro <clive@addynamo.com>
2018-09-15 17:46:19 +0200

    Added Debugging to Init Code

--- cm.rpdy.io.js
+++ cm.rpdy.io.js
@@ -1,38 +1,32 @@
-function addCSSFile(url) {
-  var stylesheet = document.createElement("link");
-  stylesheet.rel = "stylesheet";
-  stylesheet.href = url;
-  document.head.appendChild(stylesheet);
+function addCSSFile(url){
+	var stylesheet = document.createElement("link");
+	stylesheet.rel = "stylesheet";
+	stylesheet.href = url;
+	document.head.appendChild(stylesheet);
 }
-function addJSScript(url) {
-  var script = document.createElement("script");
-  script.type = "application/javascript";
-  script.src = url;
-  document.head.appendChild(script);
-}
-
-function addJQueryUi() {
-  addCSSFile(
-    "https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css"
-  );
-  addJSScript(
-    "https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"
-  );
+function addJSScript(url){
+	var script = document.createElement("script");
+	script.type = "application/javascript";
+	script.src = url;
+	document.head.appendChild(script);
 }
 
-function addEFormScript() {
-  addJSScript(
-    "https://raw.githubusercontent.com/seraphimalia/cm-eform-scripts/master/start-eform.js"
-  );
+function addJQueryUi(){
+    addCSSFile("https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css");
+    addJSScript("https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js");
 }
 
-if (
-  document.location.href.startsWith("https://cm.rpdy.io/Orders/") &&
-  document.location.href.replace("https://cm.rpdy.io/Orders/", "") > 0
-) {
-  $(document).ready(function() {
-    addJQueryUi();
-    addEFormScript();
-    addEFormButton();
-  });
+function addEFormScript(){
+    addJSScript("https://raw.githubusercontent.com/seraphimalia/cm-eform-scripts/master/start-eform.js");
+}
+
+if (document.location.href.startsWith('https://cm.rpdy.io/Orders/') && document.location.href.replace('https://cm.rpdy.io/Orders/', '') > 0) {
+    console.log("CDInjector: Incident Page Detected");
+    $(document).ready(function() {
+        addJQueryUi();
+        addEFormScript();
+        addEFormButton();
+    });
+} else {
+    console.log("CDInjector: No Incident page was detected.");
 }
