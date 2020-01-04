d5291aead9fd0c4ef8571cfd54aa098d65b6e3b8
Clive Munro <clive@addynamo.com>
2020-01-04 17:05:35 +0200

    Temp no CDN

--- cm.rpdy.io.js
+++ cm.rpdy.io.js
@@ -17,7 +17,8 @@ function addJQueryUi () {
 }
 
 function addEFormScript () {
-    addJSScript("https://cdn.jsdelivr.net/gh/seraphimalia/cm-eform-scripts@master/start-eform.js");
+    // addJSScript("https://cdn.jsdelivr.net/gh/seraphimalia/cm-eform-scripts@master/start-eform.min.js");
+    addJSScript("https://raw.githubusercontent.com/seraphimalia/cm-eform-scripts/master/start-eform.js");
     _cdlog("CDInjector: Eform Script Added.");
 }
 
